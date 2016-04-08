e5b8b1fd66470dfa022857f76b07616e0ab03d6d
christinejavier <cjavier13@ucsbalum.com>
2016-04-07 19:45:19 -0700

    Intermediate Algorithm Scripting: Sum All Primes

--- sumAllPrimes.js
+++ sumAllPrimes.js
@@ -36,3 +36,10 @@ function sumPrimes(num) {
 }
 
 console.log(sumPrimes(977));
+
+/*
+TESTS:
+sumPrimes(10) should return a number.
+sumPrimes(10) should return 17.
+sumPrimes(977) should return 73156.
+*/
